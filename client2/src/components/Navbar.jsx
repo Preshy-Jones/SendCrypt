efb4c427b58c77e11d97c168c0cf99e71db75829
Precious Adedibu <adedibuprecious@gmail.com>
2022-06-24 16:02:22 +0100

    yo

--- client2/src/components/Navbar.jsx
+++ client2/src/components/Navbar.jsx
@@ -25,7 +25,10 @@ const Navbar = () => {
           <NavBarItem key={item + index} title={item} />
         ))} */}
         {!currentAccount && (
-          <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+          <li
+            onClick={connectWallet}
+            className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]"
+          >
             Connect Wallet
           </li>
         )}
