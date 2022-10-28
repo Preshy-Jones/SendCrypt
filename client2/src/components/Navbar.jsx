7dbcb47b471436c28ae9eea3e8008b198e524a09
Precious Adedibu <adedibuprecious@gmail.com>
2022-10-28 15:39:28 +0100

    updated name

--- client2/src/components/Navbar.jsx
+++ client2/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <h1 className="text-white font-elite text-[3em] m-0">Cryptofy</h1>
+        <h1 className="text-white font-elite text-[3em] m-0">SendKrypt</h1>
         {/* <img src={logo} alt="logo" className="w-32 cursor-pointer" /> */}
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
